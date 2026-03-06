3cdcc0d5cdc45190c27e588cbe093d25f33359f3
spascarelli <pasca1989@hotmail.it>
2026-03-06 14:43:18 +0100

    Update postCreate.sh fixing relative paths

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -14,7 +14,7 @@ if [ ! -f /workspaces/SIBBiodiversityBioinformatics2025/Module2_OMAmer/working_d
 fi
 
 # Install FastOMA (repo must exist first)
-cd Module4_FastOMA/working_dir
+cd /workspaces/SIBBiodiversityBioinformatics2025/Module4_FastOMA/working_dir
 
 wget https://github.com/DessimozLab/FastOMA/archive/refs/heads/dev.zip
 unzip dev.zip
@@ -23,7 +23,7 @@ mv FastOMA-dev FastOMA
 pip install ./FastOMA[report]
 
 # clone phylogenetic repo
-cd ../../Module5_SpeciesTrees/working_dir
+cd /workspaces/SIBBiodiversityBioinformatics2025/Module5_SpeciesTrees/working_dir
 git clone https://github.com/DessimozLab/f1000_PhylogeneticTree.git
 
 echo 'export PATH=$PATH:/workspaces/SIBBiodiversityBioinformatics2025/Module5_SpeciesTrees/working_dir/f1000_PhylogeneticTree/' >> ~/.bashrc
