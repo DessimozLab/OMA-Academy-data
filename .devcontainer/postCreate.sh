4d3d5d4b55674567461b8b4e1799cdd4b0fb7d32
spascarelli <pasca1989@hotmail.it>
2026-05-04 15:09:47 +0200

    Removed old slides. Updated installation paths for env creation

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,20 +2,20 @@
 set -e
 
 # download dbs
-if [ ! -f /workspaces/SIBBiodiversityBioinformatics2025/Module4_FastOMA/working_dir/in_folder/omamerdb.h5 ]; then
+if [ ! -f /workspaces/OMA-Academy-data/Module4_FastOMA/working_dir/in_folder/omamerdb.h5 ]; then
   curl -L -O https://zenodo.org/records/17826591/files/Saccharomyceta.h5?download=1
-  cp Saccharomyceta.h5 /workspaces/SIBBiodiversityBioinformatics2025/Module4_FastOMA/working_dir/in_folder/omamerdb.h5
-  mv Saccharomyceta.h5 /workspaces/SIBBiodiversityBioinformatics2025/Module2_OMAmer/working_dir/omamer_databases/
+  cp Saccharomyceta.h5 /workspaces/OMA-Academy-data/Module4_FastOMA/working_dir/in_folder/omamerdb.h5
+  mv Saccharomyceta.h5 /workspaces/OMA-Academy-data/Module2_OMAmer/working_dir/omamer_databases/
 fi
 # METAZOA DOWNLOAD LINK WAS NOT UPDATED YET TO FACILITATE CREATION OF NEW WORKSPACE
 # WHEN YOU ARE READY, CHANGE TO THIS ONE: https://zenodo.org/records/17826591/files/Metazoa.h5?download=1
-if [ ! -f /workspaces/SIBBiodiversityBioinformatics2025/Module2_OMAmer/working_dir/omamer_databases/ ]; then
+if [ ! -f /workspaces/OMA-Academy-data/Module2_OMAmer/working_dir/omamer_databases/ ]; then
   curl -L -O https://omabrowser.org/All.Jul2024/Metazoa.h5
-  mv Metazoa.h5 /workspaces/SIBBiodiversityBioinformatics2025/Module2_OMAmer/working_dir/omamer_databases/
+  mv Metazoa.h5 /workspaces/OMA-Academy-data/Module2_OMAmer/working_dir/omamer_databases/
 fi
 
 # Install FastOMA (repo must exist first)
-cd /workspaces/SIBBiodiversityBioinformatics2025/Module4_FastOMA/working_dir
+cd /workspaces/OMA-Academy-data/Module4_FastOMA/working_dir
 
 wget https://github.com/DessimozLab/FastOMA/archive/refs/heads/dev.zip
 unzip dev.zip
@@ -24,7 +24,7 @@ mv FastOMA-dev FastOMA
 pip install ./FastOMA[report]
 
 # clone phylogenetic repo
-cd /workspaces/SIBBiodiversityBioinformatics2025/Module5_SpeciesTrees/working_dir
+cd /workspaces/OMA-Academy-data/Module5_SpeciesTrees/working_dir
 git clone https://github.com/DessimozLab/f1000_PhylogeneticTree.git
 
-echo 'export PATH=$PATH:/workspaces/SIBBiodiversityBioinformatics2025/Module5_SpeciesTrees/working_dir/f1000_PhylogeneticTree/' >> ~/.bashrc
+echo 'export PATH=$PATH:/workspaces/OMA-Academy-data/Module5_SpeciesTrees/working_dir/f1000_PhylogeneticTree/' >> ~/.bashrc
